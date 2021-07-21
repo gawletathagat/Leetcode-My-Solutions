15744e781c5cb40327f230eaccb36255f21f47c4
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2021-07-21 11:46:11 +0530

    Time: 8 ms (76.24%), Space: 9.6 MB (66.69%) - LeetHub

---
+++ word-break/word-break.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    bool rec( string s, unordered_set<string>&dict )
+    { 
+        if(dict.size()==0) return false;
+        
+        vector<bool> dp(s.size()+1,false);
+        
+        dp[0]=true;
+        
+        for(int i=1;i<=s.size();i++)
+        {
+            for(int j=i-1;j>=0;j--)
+            {
+                if(dp[j])
+                {
+                    string word = s.substr(j,i-j);
+                    if(dict.find(word)!= dict.end())
+                    {
+                        dp[i]=true;
+                        break; //next i
+                    }
+                }
+            }
+        }
+        
+        return dp[s.size()];
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict) 
+    {
+        unordered_set<string> mp(wordDict.begin() , wordDict.end()) ;
+        
+         //both are correct //mp.insert(wordDict.begin() , wordDict.end()) ;
+        //Wrong //for( int i=0; i<wordDict.size() ; i++) mp[wordDict[i]]  ;
+        
+        return rec(s, mp) ;
+    }
+};
