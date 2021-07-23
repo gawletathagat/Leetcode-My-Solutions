27374b89d08a42725283898fd62d8faec443fff2
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2021-07-23 12:01:16 +0530

    Time: 4 ms (80.59%), Space: 9 MB (25.67%) - LeetHub

--- last-stone-weight-ii/last-stone-weight-ii.cpp
+++ last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -5,6 +5,16 @@ public:
         // knapsack
         //we put the max in our bag then the (remaing  = total - max) will be our ans 
         
+//         S1 + S2 = Total_Sum_of_Array ( say S ) ...... (1)
+// Also our answer is S1 - S2 = diff ................... (2)
+
+// S1 - S2 = diff ( from (2) )
+// S1 - S2 + 2xS2 - 2xS2 = diff ( adding and subtracting 2xS2)
+// S1 + S2 - 2xS2 = diff
+// S - 2xS2 = diff ( from (1) )
+// Now, our task is to minimize this diff, which is equivalent to maximizing S2 ?
+// What can the min value of diff be? 0 right? So what can the max value of S2 be ? S/2 !
+        
         int total = accumulate( stones.begin() , stones.end() , 0) ;
         int n = stones.size() ;
         vector<vector<int>>t(n+ 1, vector<int>( total/2+ 1 , 0) ) ; // to store the max we can take
