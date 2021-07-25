f359b68f38dc8d4736309e7c92243f5be3736755
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2021-07-25 11:37:47 +0530

    Time: 160 ms (100.00%), Space: 9.9 MB (100.00%) - LeetHub

--- maximum-compatibility-score-sum/maximum-compatibility-score-sum.cpp
+++ maximum-compatibility-score-sum/maximum-compatibility-score-sum.cpp
@@ -1,17 +1,39 @@
 class Solution {
- 
 public:
-   int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
-	int ans = 0;
-	vector<int> pos;
-	for(int i=0;i<students.size();i++) pos.push_back(i);
-	do{
-		int curr = 0;
-		for(int i = 0;i<students.size(); i++)
-			for(int j=0;j<students[pos[i]].size();j++)
-				curr += (students[pos[i]][j] == mentors[i][j]);
-		ans = max(ans, curr);
-	} while(next_permutation(pos.begin(), pos.end()) );
-	return ans;
-}
+    
+    int calculate(vector<vector<int>>& students, vector<vector<int>>& mentors,int index,int t){
+        int sum = 0;
+        for(int i=0;i<students[0].size();i++)
+            if(students[index][i] == mentors[t][i])
+                sum++;
+    
+        return sum;
+    
+    }
+    
+    int dfs(vector<vector<int>>& students, vector<vector<int>>& mentors,vector<bool>& visited,int index){
+        if(index >= students.size())
+            return 0;
+    
+        int ans = INT_MIN;
+        int m = students.size();
+        for(int i=0;i<m;i++)
+        {
+            if(!visited[i])
+            {
+                visited[i] = true;
+                int temp  =  calculate(students,mentors,index,i);
+                ans = max(ans,dfs(students,mentors,visited,index+1)+temp);
+                visited[i] = false;
+            }
+        }
+        
+        return ans;
+    }
+    
+    int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
+        int m = students.size();
+        vector<bool> visited(m);
+        return dfs(students,mentors,visited,0);
+    }
 };
