867acaaff8ca71bb559d3ef71d32bcae9e9c3fc1
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2021-07-27 19:02:58 +0530

    Time: 12 ms (69.23%), Space: 9.6 MB (43.00%) - LeetHub

--- target-sum/target-sum.cpp
+++ target-sum/target-sum.cpp
@@ -1,28 +1,48 @@
 class Solution {
 public:
     
-    int solve(vector<int> &nums,int sum,int target,int index)
+    int rec( vector<int>& nums, int sum )
     {
-    
-    if(index==nums.size())
-    {
-        if(target==sum)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }  
-        int count=0;
-        count+=solve(nums,sum - nums[index],target,index + 1);
-        count+=solve(nums,sum + nums[index],target,index + 1);
+        // number of ways to get sum
+        int n = nums.size() ;
+        vector<vector<int>>t(2, vector<int>( sum + 1 , 0) ) ;
         
-        return count;
+        for( int i= 0 ; i<nums.size() ; i++) t[i%2][0] = 1;
+        for( int j = 0 ; j<sum + 1; j++) t[0][j] = 0 ;
+        
+        t[0][0] = 1;
+        
+        for( int i= 1; i<nums.size()+ 1; i++)
+        {
+            for( int j= 0; j<sum + 1; j++)
+            {
+                if( nums[i-1] <= j)
+                {
+                    t[i%2][j] = t[(i-1)%2][j] + t[(i-1)%2][j-nums[i-1] ] ;
+                }
+                else 
+                    t[i%2][j] = t[(i-1)%2 ][j ] ;
+            }
+        }
+        
+        return t[n%2][sum] ;
     }
+    
     int findTargetSumWays(vector<int>& nums, int target) 
-    {    
-        return solve(nums,0,target,0);
+    {
+//          (first_half) - (second_half )  = target 
+//          (first_half) + (second_half )  = total 
+//         ------------------------------------------
+//             first_half = (target + total)/2 ;
+        
+        // so Question become find the sum first_half in vector , now this is Knapsack
+        
+        int total = accumulate( nums.begin() , nums.end() ,  0) ;
+        
+         if( (target+ total)%2 == 1 || total < target ) return 0;
+        
+        int first_half = (target + total)/2 ;
+        
+        return rec(nums, first_half ) ;
     }
 };
