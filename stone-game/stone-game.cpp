2d75ab48dd7525f4e1b79a4ad81e9dd471f007e5
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2021-08-13 16:42:36 +0530

    Time: 24 ms (19.73%), Space: 16.5 MB (17.68%) - LeetHub

--- stone-game/stone-game.cpp
+++ stone-game/stone-game.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
+    vector<vector<int>>t ;
     
-    int rec( vector<int>&piles,vector<vector<int>>&t , int start , int end)
+    int rec( vector<int>&piles, int start , int end )
     {
-        if(start== end ) return piles[start] ;
+        if( start >= end ) return piles[start];
         
-        if( end- start == 1) return max( piles[start] , piles[end] ) ;
+        if( end - start  == 1) return max( piles[start] , piles[end ])  ; 
         
+        /// space for memoization 
         if( t[start][end] != -1) return t[start][end] ;
         
-        int first = piles[start] + min(rec(piles,t, start + 1, end - 1), rec(piles,t, start + 2, end));
-        int second = piles[end] + min(rec(piles,t, start + 1, end - 1), rec(piles,t, start, end - 2));
-        return t[start][end] = max(first, second);
+        int front =  piles[start] + min(rec( piles, start+ 1, end-1) , rec( piles, start+ 2, end));
+        
+        int back =  piles[end] + min(rec( piles, start+ 1, end-1) , rec( piles, start , end-2) );
+        
+       
+        return t[start][end]  = max( front , back) ;
+        
     }
     
-    bool stoneGame(vector<int>& piles) 
+    bool stoneGame(vector<int>& piles)
     {
-        int total = accumulate( piles.begin() , piles.end(), 0 ) ;
+        // like knapsack we have a choice to pick from last and first , we we try every way
+        // suppose we are alice so we make our sum as much as possible 
+        
+       
         int n = piles.size() ;
-        vector<vector<int>>t(n+ 1, vector<int>(n+ 1, -1) ) ;
         
-        int p1 = rec(piles, t, 0, n-1 ) ;
-        cout<<p1;
+        t.resize( n+ 1, vector<int>( n+ 1, -1) ) ;
         
-        return (total - p1) < p1 ;   
+        int max_alice_sum =  rec( piles, 0, n-1) ;
+        
+        int bob = accumulate( piles.begin() , piles.end() , 0 ) - max_alice_sum ;
+        
+        
+        cout<<max_alice_sum <<" "<<bob<<endl;
+        
+        return (max_alice_sum > bob) ? true : false ;
     }
 };
