a8cf2e06ab35e3ef56887bc2d8fc48664bb0ca16
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2021-08-17 20:07:42 +0530

    Time: 20 ms (56.45%), Space: 24.1 MB (30.20%) - LeetHub

--- max-area-of-island/max-area-of-island.cpp
+++ max-area-of-island/max-area-of-island.cpp
@@ -1,28 +1,46 @@
 class Solution {
-public: 
-    int mx,n,m;
+public:
     
-    void dfs(vector<vector<int>>& grid,int i,int j,int &s){
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!=1){
-            if(mx<s)mx=s;
-            return;
-        } 
-        s++;
-        grid[i][j]=2;
-        dfs(grid,i+1,j,s);
-        dfs(grid,i,j+1,s);
-        dfs(grid,i-1,j,s);
-        dfs(grid,i,j-1,s);
+    
+    int dfs( int i, int j , vector<vector<int>>&vis, vector<vector<int>>&grid )
+    {
+        if( i<0 || j<0 || i>=grid.size() ||j>=grid[0].size() || vis[i][j]==1 || grid[i][j]==0) return 0;
+            
+        vis[i][j] = 1;
+        int ans = 1;
+        ans = ans + dfs( i+ 1, j, vis, grid) ;
+        ans = ans + dfs( i, j+ 1, vis, grid) ;
+        ans = ans + dfs( i-1, j, vis, grid ) ;
+        ans = ans + dfs( i, j-1, vis, grid) ;
+        
+        //cout<<ans<<endl;
+        return ans ;
     }
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-        mx=0;m=grid.size();n=grid[0].size();
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                    int s=0;
-                    dfs(grid,i,j,s);
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid)
+    {
+        // M-1 DFS
+        // M-2 DSU 
+        int n = grid.size() ;
+        int m  = grid[0].size() ;
+        int max_area = 0;
+        
+        vector<vector<int>>vis(n+ 1, vector<int>(m+ 1, 0)) ;
+        
+        for( int i= 0 ; i<n ; i++)
+        {
+            for( int j = 0 ; j<m ; j++)
+            {
+                if( vis[i][j] ==0 && grid[i][j] == 1)
+                {
+                    int curr_max = 0 ;
+                    
+                    curr_max = dfs( i, j, vis, grid ) ;
+                    
+                    max_area = max( max_area , curr_max ) ;
                 }
             }
-        return mx;
+        }
+        return max_area ;  
     }
 };
