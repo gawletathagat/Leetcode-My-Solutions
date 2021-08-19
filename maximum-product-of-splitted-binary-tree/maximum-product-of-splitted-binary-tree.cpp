64c5ec2038de182415073d180a4aacb5a1d304df
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2021-08-19 12:05:00 +0530

    Time: 128 ms (39.08%), Space: 89.9 MB (26.02%) - LeetHub

--- maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
+++ maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
@@ -11,44 +11,36 @@
  */
 class Solution {
 public:
+
+int sum(TreeNode* root, vector<long long>& ans)
+{
+    if(!root) return 0;
     
-    void preorder( TreeNode* root, long int& total )
+    int ls = sum(root->left, ans);
+    
+    int rs = sum(root->right, ans);
+    
+    int sum = ls + rs + root->val;
+    
+    ans.push_back(sum);
+    
+    return sum;
+}
+
+int maxProduct(TreeNode* root)
+{
+    int mod = 1e9+7;
+    vector<long long> ans;
+    
+    sum(root, ans);
+    
+    long long maxm = LONG_MIN;
+    
+    for(int i = 0; i<ans.size()- 1; i++)
     {
-        if( root== NULL) return  ;
-        
-        total = total + root->val ;
-        
-        preorder( root->left , total ) ;
-        preorder( root->right , total) ;
+        maxm = max(maxm, ans[i]*(ans[ans.size()-1] - ans[i]));
     }
     
-    int max_prod( TreeNode* root, long int& ans , int total )
-    {
-        if( root == NULL) return 0 ;
-        
-        long int curr_sum = root->val ;
-        
-        long int left =  max_prod( root->left  , ans , total) ;
-        long int right = max_prod( root->right , ans , total) ;
-        
-        
-        ans = max({ ans , (total-left)*(left) ,(total-right)*(right) }) ;
-        
-        curr_sum = curr_sum + left + right ;
-        
-        return curr_sum ;
-        
-    }
-    
-    int maxProduct(TreeNode* root)
-    {
-        long int total = 0;
-        preorder( root, total ) ;
-        
-        long int ans = INT_MIN ;
-        
-        max_prod( root, ans, total );
-        
-        return (long)(ans)% 1000000007 ;
-    }
+    return (maxm % mod);
+}
 };
