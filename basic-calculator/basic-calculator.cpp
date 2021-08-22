7d236829219a8265617a91b81e527f807bba6e6c
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2021-08-22 19:05:21 +0530

    Time: 4 ms (98.97%), Space: 8.1 MB (43.54%) - LeetHub

--- basic-calculator/basic-calculator.cpp
+++ basic-calculator/basic-calculator.cpp
@@ -2,45 +2,49 @@ class Solution {
 public:
     int calculate(string s)
     {
-        int sum = 0 ;
-        int sign = 1;
+        //https://www.youtube.com/watch?v=HUfUzA9Ekgo
         
-        stack<int>sk  ;
-        
-        for( int i= 0 ; i<s.size() ; i++)
-        {
-            char ch = s[i] ;
+       // s = -312 + -1 -( -3+ 1+ (-8 - -1) -1) + -2
             
-            if( isdigit(ch) )
-            {
-                int val = 0 ;
-                while( i<s.size() && isdigit(s[i]) )
-                {
-                    val = val*10 + (s[i]- '0') ;
-                    i++;
-                }
-                i--;
-                val = val*sign ;
-                sum = sum + val ;
-                sign = 1;  // reset
-            }
-            else if( ch == '(' )
-            {
-                sk.push(sum) ;
-                sk.push(sign) ;
-                sign =  1 ;
-                sum = 0 ;
-            }
-            else if( ch == ')')
-            {
-                sum = sum*sk.top() ;  sk.pop() ;
-                sum = sum + sk.top() ;  sk.pop() ;
-            }
-            else if( ch == '-')
-            {
-                sign = sign*-1;
-            }
-        }
-        return sum;
+        int sum  = 0 ;
+        int sign = 1;
+        stack<int>sk ;
+        
+         for( int i= 0 ; i<s.size() ; i++)
+         {
+             char c= s[i] ;
+             
+             if( isdigit(c) )
+             {
+                 int val = 0 ;
+                 while( i<s.size() && isdigit(s[i]) )
+                 {
+                     val = val*10 + (s[i]-'0') ;
+                     i++;
+                 }
+                 i-- ;
+                 val = val*sign ;
+                 sum = sum + val ;
+                 sign = 1;
+             }
+             else if( c == '(')
+             {
+                 sk.push(sum) ;
+                 sk.push(sign) ;
+                 sum = 0; 
+                 sign = 1;
+             }
+             else if( c == ')')
+             {
+                 sum = sum*sk.top() ; sk.pop() ;
+                 sum = sum + sk.top() ;  sk.pop() ;
+             }
+             else if( c== '-')
+             {
+                 sign = sign*-1 ;
+             }
+         }
+        
+            return sum;
     }
 };
