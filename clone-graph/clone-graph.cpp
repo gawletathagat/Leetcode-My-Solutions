2b6707a889f4de9dcd629cdfeaa74eaa86587694
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2021-09-07 11:20:08 +0530

    Time: 8 ms (56.21%), Space: 8.8 MB (46.20%) - LeetHub

---
+++ clone-graph/clone-graph.cpp
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    
+     unordered_map<Node*, Node*> mp ;
+    
+    Node* cloneGraph(Node* root) 
+    { 
+        if( root== NULL) return NULL ;
+        
+        if( mp.find(root) == mp.end() ) 
+        {
+            mp[root] = new Node( root->val) ;
+            
+            for( auto x : root->neighbors)
+            {
+                mp[root] -> neighbors.push_back( cloneGraph( x) );  //vector<Node*> neighbors;
+            }
+        }
+        return mp[root] ;
+    }
+};
