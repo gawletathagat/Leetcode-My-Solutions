40d5c254e3f6685d90f61ee914fe4c54fb8c3846
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2021-09-08 20:30:37 +0530

    Time: 12 ms (16.08%), Space: 9.1 MB (14.78%) - LeetHub

--- dungeon-game/dungeon-game.cpp
+++ dungeon-game/dungeon-game.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int calculateMinimumHP(vector<vector<int>>& nums) 
+    int calculateMinimumHP(vector<vector<int>>& nums)
     {
-        int n= nums.size() ;
+        int n = nums.size() ;
         int m = nums[0].size() ;
+        vector<vector<int>>dp(n+ 1, vector<int>( m+ 1, INT_MAX) ) ;
         
-        vector<vector<int>>t( n+ 1, vector<int>(m+ 1, INT_MAX )) ;
+       dp[n-1][m] = 1;
+        dp[n][m-1] = 1;
         
-        t[n-1][m]= 1;
-        t[n][m-1] = 1;
+         for( int i = n-1; i>=0 ; i--)
+         {
+             for( int j= m-1; j>=0 ; j--)
+             {
+                int val = min( dp[i+1][j] , dp[i][j+1] ) - nums[i][j] ;
+                 
+                 dp[i][j] = max( 1, val) ;
+             }
+         }
         
-        for( int i= n-1; i>=0; i--)
-        {
-            for( int j= m-1; j>=0 ; j--)
-            {
-                int val = min( t[i+1][j] , t[i][j+1] ) - nums[i][j] ;
-                
-                t[i][j] = max( 1, val ) ;
-            }
-        }
-        
-        return t[0][0] ;
+        return dp[0][0] ;
+
         
     }
 };
