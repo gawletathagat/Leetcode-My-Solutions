251d1b856e05379d7429758f44557e7eead28fa3
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2021-09-27 14:04:29 +0530

    Time: 4 ms (74.59%), Space: 8.3 MB (61.32%) - LeetHub

--- minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
+++ minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
@@ -1,37 +1,24 @@
 class Solution {
-public:
-    
-    int rec( int start , int end , vector<int>& arr, vector<vector<int>>&t )
-    {
-        if( start >= end) return t[start][end] = 0;
-        
-        if( t[start][end] != -1) return t[start][end] ; 
-        
-        int ans = INT_MAX ;
-        for( int k = start ; k<end ; k++)
-        {
-            int first = rec( start , k , arr, t) ;
-            int second = rec( k+ 1, end , arr, t) ;
-            
-            int maxi_first = *max_element(arr.begin()+ start, arr.begin()+ k+ 1) ;
-            int maxi_second = *max_element(arr.begin()+ k+ 1, arr.begin()+ end+ 1) ;
-            
-            int temp = first + second + maxi_first* maxi_second;
-            
-            ans = min( ans , temp) ;
-        }
-        return t[start][end] = ans ;
+private:
+    int getMax(vector<int> &a, int i, int j) {
+        if(i >= a.size() || i>j) return 0;
+        int mx = a[i];
+        for(int k = i; k <= j; k++) mx = max(mx, a[k]);
+        return mx;
     }
-    
-    int mctFromLeafValues(vector<int>& arr) 
-    {
-        //MCM so we use recursion but that is Bruit force
-        // i, j, k o(n3) , this Q can be done with stack in O(n) 
-        // dp[i, j] = dp[i, k] + dp[k + 1, j] + max(A[i, k]) * max(A[k + 1, j])
-        
-        int n = arr.size() ;
-        vector<vector<int>>t(n+ 1, vector<int>( n+ 1, -1) ) ;
-        
-        return rec( 0 , n-1, arr, t) ;
+    int compute(vector<int> &a, int i, int j) {
+        if(i > j) return 0;
+        int mxi = i;
+        for(int k = i; k <= j; k++) {
+            if(a[k] > a[mxi]) mxi=k;
+        }
+        int leftVal = compute(a,i,mxi-1);
+        int rightVal = compute(a,mxi+1,j);
+        int mx = a[mxi];
+        return leftVal + rightVal + mx * getMax(a,i,mxi-1) + mx * getMax(a,mxi+1,j);
+    }
+public:
+    int mctFromLeafValues(vector<int>& arr) {
+        return compute(arr, 0, arr.size()-1);
     }
 };
