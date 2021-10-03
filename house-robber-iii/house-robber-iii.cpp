ea213ac4c0aa1a56b2af5e8a9e8cf186638f44f8
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2021-10-03 17:48:37 +0530

    Time: 12 ms (84.88%), Space: 22.3 MB (63.23%) - LeetHub

--- house-robber-iii/house-robber-iii.cpp
+++ house-robber-iii/house-robber-iii.cpp
@@ -1,27 +1,35 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    
-    
-    pair<int,int> dfs( TreeNode* root )
+    pair<int,int> dfs(TreeNode* root )
     {
         if( root== NULL) return {0,0} ;
         
-        pair<int,int> left = dfs( root->left) ;
-        pair<int,int> right = dfs( root->right) ;
+        pair<int,int>left = dfs( root->left) ;
+        pair<int,int>right = dfs( root->right) ;
         
-        int include_root = root->val + left.second + right.second ;
+        int choice1 = root->val + left.second + right.second  ; // not select from left and right tree
         
-        int not_include_root = max( left.first, left.second) + max( right.first , right.second) ;
+        int choice2 = max(left.first, left.second) + max( right.first, right.second) ;
         
-        return {include_root, not_include_root} ;
+        return {choice1, choice2} ;
     }
     
-    int rob(TreeNode* root) 
+    int rob(TreeNode* root)
     {
-        // first == include root , second == not include root
-       pair<int,int> head = dfs( root ) ;
+        // first = include, second = not include root
+        pair<int,int>decide = dfs( root );
         
-        return max( head.first , head.second ) ;
+        return max( decide.first , decide.second) ;
     }
 };
