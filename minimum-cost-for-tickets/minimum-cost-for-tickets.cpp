bc6bd2e88d10541eef60b60b096c7045dadfd23d
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2021-10-06 19:30:28 +0530

    Time: 4 ms (72.10%), Space: 9.6 MB (55.17%) - LeetHub

--- minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
     
-    int dp[366];
-    
-    int rec( int i , vector<int>&days , vector<int>&costs )
+    int rec( vector<int>&days, vector<int>&costs, int i, vector<int>&dp )
     {
-        int n = days.size() ;
+        if( i== days.size() ) return 0;
+        if( dp[i] != -1) return dp[i] ;
         
-        if( i == days.size() ) return 0 ;
-         if( dp[i] != -1) return dp[i] ;
+        int choice1 = costs[0] ;
+        choice1 = choice1 + rec( days, costs, i+ 1, dp) ;
         
-        int choice_1 = costs[0] + rec( i+ 1, days, costs) ;
-        
-        int choice_2 = costs[1] ;
+        int choice2 = costs[1] ;
         int j = i ;
-        while(j<n && days[j] - days[i] < 7 ) j++;
-        choice_2 = choice_2 + rec( j , days, costs ) ;
+        while( j<days.size() && days[j]- days[i] < 7) j++;
         
-        int choice_3 = costs[2] ;
-         j = i ;
-        while( j<n && days[j] - days[i] <30) j++;
-        choice_3 = choice_3 + rec( j, days, costs) ;
+        choice2 = choice2 + rec( days, costs, j , dp) ;
         
-        return dp[i] = min({ choice_1, choice_2 , choice_3 } ) ;
         
+        int choice3 = costs[2] ;
+        j = i ;
+        while( j<days.size() && days[j] - days[i] < 30) j++;
+        
+        choice3 = choice3 + rec( days, costs, j, dp) ;
+        
+        return dp[i] = min({choice1, choice2, choice3} ) ;
     }
     
-    
-    int mincostTickets(vector<int>& days, vector<int>& costs) 
+    int mincostTickets(vector<int>& days, vector<int>& costs)
     {
+       vector<int>dp(366,-1) ;
         
-      memset(dp, -1, sizeof(dp) ) ;
-        
-        sort( days.begin() , days.end() ) ;
-        return rec( 0, days, costs) ;
+        return rec( days, costs, 0, dp) ;
     }
 };
