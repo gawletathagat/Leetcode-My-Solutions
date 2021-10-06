5935cdf77c42cbb7140d3cd54d2b09d8e2edb52b
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2021-10-06 23:02:22 +0530

    Time: 8 ms (60.44%), Space: 9 MB (49.19%) - LeetHub

--- dungeon-game/dungeon-game.cpp
+++ dungeon-game/dungeon-game.cpp
@@ -4,23 +4,23 @@ public:
     {
         int n = nums.size() ;
         int m = nums[0].size() ;
-        vector<vector<int>>dp(n+ 1, vector<int>( m+ 1, INT_MAX) ) ;
         
-       dp[n-1][m] = 1;
-        dp[n][m-1] = 1;
+        vector<vector<int>>dp(n+ 1, vector<int>(m + 1, INT_MAX ) ) ;
         
-         for( int i = n-1; i>=0 ; i--)
-         {
-             for( int j= m-1; j>=0 ; j--)
-             {
-                int val = min( dp[i+1][j] , dp[i][j+1] ) - nums[i][j] ;
-                 
-                 dp[i][j] = max( 1, val) ;
-             }
-         }
+        dp[n-1][m] = 1 ;
+        dp[n][m-1] = 1 ;
+        
+       for( int i= n-1; i>=0 ; i--)
+       {
+           for( int j = m-1; j>=0 ; j--)
+           {
+               int val = min( dp[i+1][j] , dp[i][j+1] )  - nums[i][j] ;
+               
+               dp[i][j] = max( 1, val) ;
+           }
+       }
+        
         
         return dp[0][0] ;
-
-        
     }
 };
