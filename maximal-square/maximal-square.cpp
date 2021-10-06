29c90ad77320ea58315e8fbee8a5f360716d9a64
Tathagat Gawle <68445028+gawletathagat@users.noreply.github.com>
2021-10-06 18:14:44 +0530

    Time: 24 ms (75.54%), Space: 11.8 MB (45.96%) - LeetHub

--- maximal-square/maximal-square.cpp
+++ maximal-square/maximal-square.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& nums) 
+    int maximalSquare(vector<vector<char>>& matrix) 
     {
-        int n = nums.size() ;
-        int m = nums[0].size() ;
+        int n = matrix.size() ;
+        int m = matrix[0].size() ;
         
-        vector<vector<int>>t(n+ 1, vector<int>(m+ 1, 0) ) ;
+        vector<vector<int>>dp(n+1, vector<int>( m+1, 0) ) ;
         
-        for( int i= 0 ; i<n ; i++)
-            t[i][0] = nums[i][0]-'0' ;
+        for( int i= 0 ; i<n; i++) dp[i][0] = matrix[i][0]-'0' ;
         
-        for( int j= 0 ; j<m; j++)
+        for( int j= 0 ; j<m ; j++) dp[0][j] = matrix[0][j]-'0' ;
+         
+        
+        for( int i= 1 ; i<n; i++)
         {
-            t[0][j] = nums[0][j]-'0' ;
-        }
-        
-        for( int i= 1; i<n; i++)
-        {
-            for( int j = 1; j<m; j++)
+            for( int j= 1 ;j<m ; j++)
             {
-                if( nums[i][j] != '0')
-                t[i][j] = min({ t[i-1][j] , t[i][j-1] , t[i-1][j-1]}) + 1 ;
+                if( matrix[i][j] == '1')
+                {
+                    dp[i][j] = matrix[i][j]-'0' + min({dp[i-1][j] , dp[i][j-1] , dp[i-1][j-1]} ) ;
+                }
             }
         }
         
-        int maxi = 0 ;
-        for( int i= 0 ; i<n; i++)
+        int max_area = 0 ;
+        for(int i= 0 ; i<n ; i++ )
         {
-            for( int j = 0 ; j<m ; j++)
+            for( int j= 0 ; j<m ; j++) 
             {
-                if( maxi < t[i][j] ) maxi = t[i][j] ;
+                if( max_area < dp[i][j] )  max_area = dp[i][j] ;
             }
         }
+        return max_area*max_area ;
         
-        return maxi*maxi;
     }
 };
